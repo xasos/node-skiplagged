3ad9a7fd93af2f17599301c51aa8fbb45259864a
Niraj Pant <nirajpant7@gmail.com>
2015-01-09 22:17:58 -0600

    add travis ci

--- index.js
+++ index.js
@@ -3,15 +3,35 @@
 var cheerio = require('cheerio');
 var request = require('request');
 
+var BASE_URL = 'https://skiplagged.com/';
+
 function Skiplagged(opts) {
 	opts = opts || {};
 };
 
 /**
- * Gets flights for a specific date
+ * Gets flights for a specific date and cities
  */
-Skiplagged.prototype.flights = function(to, from, opts) {
+Skiplagged.prototype.flights = function(to, from, toDate, fromDate, opts) {
 	opts = opts || {};
+
+	var flightUrl;
+	// to do: standardize date
+	flightUrl += 'src?=' + 
+	from + '&dst=' + 
+	to + '&when=' + 
+	fromDate + '&whenBack=' + 
+	toDate + '&sort=cost';
+	//https://skiplagged.com/?src=JFK&dst=ORD&when=2015-02-24&whenBack=2015-02-26&sort=cost
 };
+
+function getFlights(CONSTRUCTED_URL) {
+	this.url = CONSTRUCTED_URL;
+	request(url, function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+    		console.log(body) 
+  		}
+	});
+}
 
 module.exports = Skiplagged;
