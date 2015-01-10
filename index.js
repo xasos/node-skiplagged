3aa9748103148d1d8bb02ba3bb9205d8b16d4594
Niraj Pant <nirajpant7@gmail.com>
2015-01-09 21:29:40 -0600

    add cheerio

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
 
+var cheerio = require('cheerio');
+var request = require('request');
+
 function Skiplagged(opts) {
 	opts = opts || {};
-}
+};
 
 /**
  * Gets flights for a specific date
  */
-
 Skiplagged.prototype.flights = function(to, from, opts) {
 	opts = opts || {};
 };
