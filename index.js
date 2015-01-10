893b31e375371bf294ffce7d341489ff17ee503d
Niraj Pant <nirajpant7@gmail.com>
2015-01-09 21:20:22 -0600

    add package

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function Skiplagged(opts) {
+	opts = opts || {};
+}
+
+/**
+ * Gets flights for a specific date
+ */
+
+Skiplagged.prototype.flights = function(to, from, opts) {
+	opts = opts || {};
+};
+
+module.exports = Skiplagged;
